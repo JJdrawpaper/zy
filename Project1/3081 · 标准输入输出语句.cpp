cb119f5b8fcbf83ed441a3e796ca7d466cac4c29
JJdrawpaper <3368386952@qq.com>
2026-01-17 23:53:40 +0800

    Project1/3081 · 标准输入输出语句.cpp

---
+++ Project1/3081 · 标准输入输出语句.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int max(int age,char name){
+    return 0;
+}
+int main(){
+    char name[10];
+    int age=0;
+    cin >> name;
+    cin >> age;
+    cout << name <<  "is"  << age <<  "years old."  << endl;
+     return 0;
+}
