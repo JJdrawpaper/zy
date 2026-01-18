3ed7d459d8daad813d9a9672a1550af292eb3f15
JJdrawpaper <3368386952@qq.com>
2026-01-18 19:54:40 +0800

    qa

---
+++ Project1/qa.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int max(int n, int i) {
+    return 0;
+}
+int main() {
+    int n = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << n << " is " << i << " now." << endl;
+    }
+    return 0;
+}
